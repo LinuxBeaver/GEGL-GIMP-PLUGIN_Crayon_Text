2de08fd7d244076a08d57515f3ca6b981f497e3c
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-09-26 20:15:48 -0400

    Add files via upload

--- compile_crayon_text/crayontext.c
+++ compile_crayon_text/crayontext.c
@@ -41,13 +41,18 @@ property_double (spread, _("Crayon Effect"), 5.5)
 
 property_double (intensity, _("Intensity control"), 20.0)
    description (_("Control the intensity of the crayon text. Lower values increase the intensity. Higher values will make the crayon text even more thin. Use high values on very bold fonts and low values on normal size fonts."))
-   value_range (15.0, 80.0)
-   ui_range    (15.0, 80.0)
+   value_range (15.0, 100.0)
+   ui_range    (15.0, 100.0)
    ui_gamma    (2.0)
   ui_steps      (0.01, 0.50)
 
 property_color  (color, _("Color"), "#ff0000")
     description (_("Color of the drawn text"))
+  ui_meta     ("sensitive", "! originalcolor")
+
+
+property_boolean (originalcolor, _("Use original color instead of color overlay"), FALSE)
+  description    (_("Check to use original color on crayon text instead of color overlay"))
 
 #else
 
@@ -57,80 +62,137 @@ property_color  (color, _("Color"), "#ff0000")
 
 #include "gegl-op.h"
 
+
+typedef struct
+{
+GeglNode *input;
+GeglNode *output;
+GeglNode *thecolor;
+GeglNode *thecolorremover; 
+GeglNode *blurfix; 
+GeglNode *subtract;
+GeglNode *spread; 
+GeglNode *bevel;
+GeglNode *color;
+GeglNode *idref;
+GeglNode *idref2;
+GeglNode *white;
+GeglNode *multiply;
+} State; 
+
+
+
 static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
-  GeglNode *input, *string1, *string2, *string3, *subtract, *spread, *bevel, *idref, *color, *output;
+  GeglProperties *o = GEGL_PROPERTIES (operation);
 
-  input    = gegl_node_get_input_proxy (gegl, "input");
-  output   = gegl_node_get_output_proxy (gegl, "output");
+  State *state = o->user_data = g_malloc0 (sizeof (State));
+
+  state->input    = gegl_node_get_input_proxy (gegl, "input");
+  state->output   = gegl_node_get_output_proxy (gegl, "output");
+
 
 #define muhcolor \
 " color-overlay value=#36ff00 "\
 
-  string1 = gegl_node_new_child (gegl,
+  state->thecolor = gegl_node_new_child (gegl,
                                   "operation", "gegl:gegl", "string", muhcolor,
                                   NULL);
 
 #define muhcolorremoval \
 " median-blur radius=0 color-to-alpha color=#36ff00 transparency-threshold=0.211755 "\
 
-  string2 = gegl_node_new_child (gegl,
+  state->thecolorremover = gegl_node_new_child (gegl,
                                   "operation", "gegl:gegl", "string", muhcolorremoval,
                                   NULL);
 #define muhblur \
 " gaussian-blur std-dev-x=0.5 std-dev-y=0.5 median-blur radius=0 "\
 
-  string3 = gegl_node_new_child (gegl,
+  state->blurfix = gegl_node_new_child (gegl,
                                   "operation", "gegl:gegl", "string", muhblur,
                                   NULL);
 
-  subtract = gegl_node_new_child (gegl,
+  state->subtract = gegl_node_new_child (gegl,
                                   "operation", "gegl:subtract", 
                                   NULL);
 
-  spread = gegl_node_new_child (gegl,
+  state->spread = gegl_node_new_child (gegl,
                                   "operation", "gegl:noise-spread", 
                                   NULL);
 
-  bevel = gegl_node_new_child (gegl,
+  state->bevel = gegl_node_new_child (gegl,
                                   "operation", "lb:bevel", "radius1", 10.0, "bevel1", 90.0,  
                                   NULL);
 
-  color = gegl_node_new_child (gegl,
+  state->color = gegl_node_new_child (gegl,
                                   "operation", "gegl:color-overlay", 
                                   NULL);
 
-  idref = gegl_node_new_child (gegl,
+  state->idref = gegl_node_new_child (gegl,
                                   "operation", "gegl:nop", 
                                   NULL);
 
-  gegl_node_link_many (input, string1, idref, subtract, string2, spread, string3, color, output, NULL);
-  gegl_node_link_many (idref, bevel,  NULL);
-  gegl_node_connect_from (subtract, "aux", bevel, "output");
-/*This graph is straight forward, all the nodes are being called and bevel is being put inside the subtract blend mode.*/
+  state->idref2 = gegl_node_new_child (gegl,
+                                  "operation", "gegl:nop", 
+                                  NULL);
 
- gegl_operation_meta_redirect (operation, "spread", spread, "amount-x"); 
- gegl_operation_meta_redirect (operation, "spread", spread, "amount-y"); 
- gegl_operation_meta_redirect (operation, "intensity", bevel, "bevel2"); 
- gegl_operation_meta_redirect (operation, "color", color, "value"); 
+  state->multiply = gegl_node_new_child (gegl,
+                                  "operation", "gegl:multiply", 
+                                  NULL);
 
+#define colorwhite \
+" color-overlay value=#ffffff "\
+
+ state->white = gegl_node_new_child (gegl,
+                                  "operation", "gegl:gegl", "string", colorwhite,
+                                  NULL);
+
+} /* attach */
+
+static void update_graph (GeglOperation *operation)
+{
+  GeglProperties *o = GEGL_PROPERTIES (operation);
+  State *state = o->user_data;
+  if (!state) return;
+
+  if (o->originalcolor)
+  {
+  gegl_node_link_many (state->input, state->idref2, state->thecolor, state->idref, state->subtract, state->thecolorremover, state->spread, state->blurfix, state->white, state->multiply, state->output, NULL);
+  gegl_node_link_many (state->idref, state->bevel,  NULL);
+  gegl_node_connect_from (state->subtract, "aux", state->bevel, "output");
+  gegl_node_connect_from (state->multiply, "aux", state->idref2, "output");
+  }
+  else
+  {
+  gegl_node_link_many (state->input, state->thecolor, state->idref, state->subtract, state->thecolorremover, state->spread, state->blurfix, state->color, state->output, NULL);
+  gegl_node_link_many (state->idref, state->bevel,  NULL);
+  gegl_node_connect_from (state->subtract, "aux", state->bevel, "output");
+  }
+
+  gegl_operation_meta_redirect (operation, "spread", state->spread, "amount-x"); 
+  gegl_operation_meta_redirect (operation, "spread", state->spread, "amount-y"); 
+  gegl_operation_meta_redirect (operation, "intensity", state->bevel, "bevel2"); 
+  gegl_operation_meta_redirect (operation, "color", state->color, "value"); 
 }
 
 static void
 gegl_op_class_init (GeglOpClass *klass)
 {
   GeglOperationClass *operation_class;
-
+GeglOperationMetaClass *operation_meta_class = GEGL_OPERATION_META_CLASS (klass);
   operation_class = GEGL_OPERATION_CLASS (klass);
 
   operation_class->attach = attach;
+  operation_meta_class->update = update_graph;
 
   gegl_operation_class_set_keys (operation_class,
-    "name",        "lb:crayon",
+    "name",        "lb:crayon", 
     "title",       _("Crayon/Hand Drawn Text"),
     "reference-hash", "fseak3crayosn234421gyzjhee",
-    "description", _("Give your text the appearance of a crayon drawing or hand drawn text. Try using a semi bond font that resemble human hand drawings with this. "),
+    "description", _("Give your text the appearance of a crayon drawing or hand drawn text. Try using a semi bold font that resemble human hand drawings with this. "),
+    "gimp:menu-path", "<Image>/Filters/Text Styling",
+    "gimp:menu-label", _("Crayon Text..."),
     NULL);
 }
 
